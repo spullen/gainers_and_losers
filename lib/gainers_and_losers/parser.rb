ce37b9fdd3425d1488ecaf3163ce240a3dd3e360
Scott Pullen <s.pullen05@gmail.com>
2012-05-22 12:35:08 -0400

    starting to parse

--- lib/gainers_and_losers/parser.rb
+++ lib/gainers_and_losers/parser.rb
@@ -14,7 +14,18 @@ module GainersAndLosers
     private
     
       def parse_entries
-        []
+        doc = retrieve_document
+
+        entries = []
+        doc.xpath("//div[@id='yfitp']/table/tbody/tr").each do |row|
+          row = Nokogiri::XML.fragment(row)
+        end
+        
+        entries
+      end
+      
+      def retrieve_document
+        Nokogiri::HTML(open(@url))
       end
   
   end
