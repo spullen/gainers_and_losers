2f8fca33f5e299047674199965f1fe33bf18d979
Scott Pullen <s.pullen05@gmail.com>
2012-05-22 13:22:51 -0400

    parsed out data from page

--- lib/gainers_and_losers/parser.rb
+++ lib/gainers_and_losers/parser.rb
@@ -14,18 +14,28 @@ module GainersAndLosers
     private
     
       def parse_entries
-        doc = retrieve_document
-
         entries = []
-        doc.xpath("//div[@id='yfitp']/table/tbody/tr").each do |row|
+        document.xpath("//div[@id='yfitp']/table/tbody/tr").each do |row|
           row = Nokogiri::XML.fragment(row)
+          data = row.search("td")
+          
+          symbol                  = data[0].content
+          name                    = data[1].content.gsub('  ', ' ')
+          price                   = data[2].content.split.first
+          last_trade_time         = data[2].content.gsub(price, '').strip
+          direction               = data[3].search("img").first['alt']
+          price_change            = data[3].content.split.first
+          price_change_percentage = data[3].content.split[1].gsub('(', '').gsub(')', '')
+          volume                  = data[4].content
+          
+          entries << GainersAndLosers::Entry.new(symbol, name, price, last_trade_time, direction, price_change, price_change_percentage, volume)
         end
         
         entries
       end
       
-      def retrieve_document
-        Nokogiri::HTML(open(@url))
+      def document
+        @document ||= Nokogiri::HTML(open(@url))
       end
   
   end
