7f70f6b7a2e4d269b1bceb623aa948c2b347d822
Scott Pullen <s.pullen05@gmail.com>
2012-05-27 19:28:09 -0400

    adding a test for when no symbols are given

--- spec/gainers_and_losers/quotes_spec.rb
+++ spec/gainers_and_losers/quotes_spec.rb
@@ -9,6 +9,7 @@ describe GainersAndLosers::Quotes do
     
     @q1 = GainersAndLosers::Quotes.new('INTC')
     @q2 = GainersAndLosers::Quotes.new('INTC', 'F')
+    @q3 = GainersAndLosers::Quotes.new()
   end
   
   describe '#entries' do
@@ -19,6 +20,12 @@ describe GainersAndLosers::Quotes do
       
       @q2.stub(:get_xml).and_return(Nokogiri::XML(File.open(File.expand_path('../../files/results.xml', __FILE__))))
       @q2.entries.should == [@quote1, @quote2]
+    end
+    
+    context 'given no symbols to lookup' do
+      it 'returns an empty array' do
+        @q3.entries.should == []
+      end
     end
   end
 end
